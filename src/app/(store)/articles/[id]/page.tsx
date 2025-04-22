608b4a5b5414ccbaa0d6e7bbac57ebdee9bdaf84
Parisa Esmaeilpour <esmaeilpour.parisa@gmail.com>
2025-04-23 00:08:58 +0330

    fix(articles): add responsive styles to component.

--- src/app/(store)/articles/[id]/page.tsx
+++ src/app/(store)/articles/[id]/page.tsx
@@ -20,33 +20,31 @@ export default async function BlogPage({ params }: BlogPageProps) {
   const blog = res.data;
 
   return (
-    <div className="p-16 border-t-2 border-secondary mx-4">
+    <div className="p-8 md:p-16 border-t-2 border-secondary mx-4">
       <h1 className="text-xl md:text-2xl font-bold text-gray-800">
         {blog.title}
       </h1>
 
-      <div className="grid grid-cols-5 gap-20">
-        <div className="mb-8 col-span-3 self-center">
+      <div className="flex flex-col-reverse lg:grid lg:grid-cols-5 lg:gap-6 xl:gap-20 mb-8">
+        <div className="mb-8 col-span-3 self-center mt-8 ">
           <p className="text-xl font-semibold text-primary">
             {blogLocalization.abstract}
           </p>
-          <div
-            className="max-w-none text-justify leading-loose"
-            dangerouslySetInnerHTML={{ __html: blog.summary }}
-          />
+          <div className="max-w-none text-justify leading-loose">
+            {blog.summary}
+          </div>
         </div>
-        <div className="flex justify-end col-span-2">
-          <img src={`${BASE_url}${blog.image}`} alt={blog.title} className="" />
+        <div className="flex justify-end mt-4 col-span-2">
+          <img src={`${BASE_url}${blog.image}`} alt={blog.title} />
         </div>
       </div>
       <div>
         <p className="text-xl font-semibold text-primary">
           {blogLocalization.content}
         </p>
-        <div
-          className="max-w-none text-justify leading-loose"
-          dangerouslySetInnerHTML={{ __html: blog.content }}
-        />
+        <div className="max-w-none text-justify leading-loose">
+          {blog.content}
+        </div>
       </div>
     </div>
   );
